218073e5d9cbb404da0565fbeb566068dd229908
Igor Arkhipov <duffin@yandex.ru>
2014-04-19 17:37:21 +0400

    Created 4 flipping containers

--- dclock/dclock.js
+++ dclock/dclock.js
@@ -1,16 +1,25 @@
 /**
  * Created by alex on 19/04/14.
  */
+var currentDate = new Date();
+
+var currentMin = currentDate.getMinutes().toString();
+if (currentMin.length < 2) currentMin = "0" + currentMin;
+//    this.hr = t.getHours();
+
+//console.log(currentDate);
+
+
 setInterval(function () {
-    counter()
+    minuteSecondDigit();
 }, 1000);
 
-function counter() {
+function minuteSecondDigit() {
     $("body").removeClass("play");
-    var aa = $("ul.counter li.active");
+    var aa = $("ul.minuteSecondDigit li.active");
 
     if (aa.html() == undefined) {
-        aa = $("ul.counter li").eq(0);
+        aa = $("ul.minuteSecondDigit li").eq(currentDate.getMinutes() % 10);
         aa.addClass("before")
             .next("li")
             .addClass("active")
@@ -18,15 +27,47 @@ function counter() {
             .addClass("play");
     }
     else if (aa.is(":last-child")) {
-        $("ul.counter li").removeClass("before");
+        $("ul.minuteSecondDigit li").removeClass("before");
         aa.addClass("before").removeClass("active");
-        aa = $("ul.counter li").eq(0);
+        aa = $("ul.minuteSecondDigit li").eq(currentDate.getMinutes() % 10);
+        aa.addClass("active")
+            .closest("body")
+            .addClass("play");
+        minuteFirstDigit();
+    }
+    else {
+        $("ul.minuteSecondDigit li").removeClass("before");
+        aa.addClass("before")
+            .removeClass("active")
+            .next("li")
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
+    }
+}
+
+function minuteFirstDigit() {
+    $("body").removeClass("play");
+    var aa = $("ul.minuteFirstDigit li.active");
+
+    if (aa.html() == undefined) {
+        aa = $("ul.minuteFirstDigit li").eq(currentDate.getMinutes() / 10 >> 0);
+        aa.addClass("before")
+            .next("li")
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
+    }
+    else if (aa.is(":last-child")) {
+        $("ul.minuteFirstDigit li").removeClass("before");
+        aa.addClass("before").removeClass("active");
+        aa = $("ul.minuteFirstDigit li").eq(currentDate.getMinutes() / 10 >> 0);
         aa.addClass("active")
             .closest("body")
             .addClass("play");
     }
     else {
-        $("ul.counter li").removeClass("before");
+        $("ul.minuteFirstDigit li").removeClass("before");
         aa.addClass("before")
             .removeClass("active")
             .next("li")
