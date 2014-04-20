a1f1d72a180648c17be6c1d93512a5421bd7c9ef
Alexander Ariskin <uxcoder@yandex.com>
2014-04-20 10:51:17 +0200

    Added test.html

--- dclock/dclock.js
+++ dclock/dclock.js
@@ -1,16 +1,14 @@
 /**
  * Created by alex on 19/04/14.
  */
-setInterval(function () {
-    counter()
-}, 1000);
+setInterval(function () { flipMinuteSecondDigit() }, 1000);
 
-function counter() {
+function flipMinuteSecondDigit() {
     $("body").removeClass("play");
-    var aa = $("ul.counter li.active");
+    var aa = $("ul.minuteSecondDigit li.active");
 
-    if (aa.html() == undefined) {
-        aa = $("ul.counter li").eq(0);
+    if (aa.html() === undefined) {
+        aa = $("ul.minuteSecondDigit li").eq(0);
         aa.addClass("before")
             .next("li")
             .addClass("active")
@@ -18,15 +16,115 @@ function counter() {
             .addClass("play");
     }
     else if (aa.is(":last-child")) {
-        $("ul.counter li").removeClass("before");
+        $("ul.minuteSecondDigit li").removeClass("before");
         aa.addClass("before").removeClass("active");
-        aa = $("ul.counter li").eq(0);
+        aa = $("ul.minuteSecondDigit li").eq(0);
+        aa.addClass("active")
+            .closest("body")
+            .addClass("play");
+
+        flipMinuteFirstDigit();
+    }
+    else {
+        $("ul.minuteSecondDigit li").removeClass("before");
+        aa.addClass("before")
+            .removeClass("active")
+            .next("li")
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
+    }
+}
+
+function flipMinuteFirstDigit() {
+    $("body").removeClass("play");
+    var aa = $("ul.minuteFirstDigit li.active");
+
+    if (aa.html() === undefined) {
+        aa = $("ul.minuteFirstDigit li").eq(0);
+        aa.addClass("before")
+            .next("li")
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
+    }
+    else if (aa.is(":last-child")) {
+        $("ul.minuteFirstDigit li").removeClass("before");
+        aa.addClass("before").removeClass("active");
+        aa = $("ul.minuteFirstDigit li").eq(0);
+        aa.addClass("active")
+            .closest("body")
+            .addClass("play");
+
+        flipHourSecondDigit();
+    }
+    else {
+        $("ul.minuteFirstDigit li").removeClass("before");
+        aa.addClass("before")
+            .removeClass("active")
+            .next("li")
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
+    }
+}
+
+
+function flipHourSecondDigit() {
+    $("body").removeClass("play");
+    var aa = $("ul.hourSecondDigit li.active");
+
+    if (aa.html() === undefined) {
+        aa = $("ul.hourSecondDigit li").eq(0);
+        aa.addClass("before")
+            .next("li")
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
+    }
+    else if (aa.is(":last-child")) {
+        $("ul.hourSecondDigit li").removeClass("before");
+        aa.addClass("before").removeClass("active");
+        aa = $("ul.hourSecondDigit li").eq(0);
+        aa.addClass("active")
+            .closest("body")
+            .addClass("play");
+
+        flipHourFirstDigit();
+    }
+    else {
+        $("ul.hourSecondDigit li").removeClass("before");
+        aa.addClass("before")
+            .removeClass("active")
+            .next("li")
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
+    }
+}
+
+function flipHourFirstDigit() {
+    $("body").removeClass("play");
+    var aa = $("ul.hourFirstDigit li.active");
+
+    if (aa.html() === undefined) {
+        aa = $("ul.hourFirstDigit li").eq(0);
+        aa.addClass("before")
+            .next("li")
+            .addClass("active")
+            .closest("body")
+            .addClass("play");
+    }
+    else if (aa.is(":last-child")) {
+        $("ul.hourFirstDigit li").removeClass("before");
+        aa.addClass("before").removeClass("active");
+        aa = $("ul.hourFirstDigit li").eq(0);
         aa.addClass("active")
             .closest("body")
             .addClass("play");
     }
     else {
-        $("ul.counter li").removeClass("before");
+        $("ul.hourFirstDigit li").removeClass("before");
         aa.addClass("before")
             .removeClass("active")
             .next("li")
